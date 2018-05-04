944fb381338ac4345016296e19c7e6edb9e5c3ac
Anthony Torres <atorres@g2crowd.com>
2018-05-04 00:00:36 -0500

    Enabled the Django admin for the user profile model

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -11,8 +11,10 @@ class UserProfileManager(BaseUserManager):
         if not email:
             raise ValueError('Users must have an email address.')
 
-        email = self.normalize_email(email)
-        user = self.model(email=email, name=name)
+        user = self.model(
+            email=self.normalize_email(email),
+            name=name,
+        )
 
         user.set_password(password)
         user.save(using=self._db)
@@ -21,7 +23,7 @@ class UserProfileManager(BaseUserManager):
     def create_superuser(self, email, name, password):
         """Creates and saves a superuser with the given details"""
 
-        user = self.create_user(email, name, password)
+        user = self.create_user(email, password=password, name=name)
 
         user.is_admin = True
         user.save(using=self._db)
@@ -29,29 +31,31 @@ class UserProfileManager(BaseUserManager):
 
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
-    """Represent a user profile inside our system"""
-
-    email = models.EmailField(max_length=255, unique=True)
+    email = models.EmailField(verbose_name='email address', max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
 
     objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name']
 
-    def get_full_name(self):
-        """Used to get a users full name."""
+    def __str__(self):
+        return self.email
 
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    def get_full_name(self):
         return self.name
 
     def get_short_name(self):
-        """Used to get a users short name."""
-
         return self.name
 
-    def __str__(self):
-        """Django uses this when it needs to convert the object to a string"""
-
-        return self.email
+    @property
+    def is_staff(self):
+        return self.is_admin
