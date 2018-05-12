74eadb5a6651998854160cb146cd027c90a14e0e
Anthony Torres <atorres@g2crowd.com>
2018-05-11 22:57:08 -0500

    Git ignore .idea

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -2,12 +2,16 @@ from django.shortcuts import render
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from . import serializers
 
 
 class HelloApiView(APIView):
 
-    def get(self, request):
+    serializer_class = serializers.HelloSerializer
 
+    def get(self, request):
         an_apiview = [
             'Uses HTTP methods as function (get, post, patch, put, delete',
             'It is similar to a traditional Django view',
@@ -17,4 +21,21 @@ class HelloApiView(APIView):
 
         return Response({'message': 'Hello!', 'an_apiview': an_apiview})
 
+    def post(self, request):
+        serializer = serializers.HelloSerializer(data=request.data)
 
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}'.format(name)
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        return Response({'method': 'put'})
+
+    def patch(self, request, pk=None):
+        return Response({'method': 'patch'})
+
+    def delete(self, request, pk=None):
+        return Response({'method': 'delete'})
